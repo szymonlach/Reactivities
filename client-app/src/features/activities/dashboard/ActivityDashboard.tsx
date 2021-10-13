05a66ba8bc6b232446dce380363877b8ccd36c81
Szymon Lach <szymom.sp1@gmail.com>
2021-10-13 19:54:04 +0200

    End of section 8

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Grid, GridColumn } from "semantic-ui-react";
 import ActivityList from "./ActivityList";
 import ActivityDetails from "../details/ActivityDetails"
 import ActivityForm from "../form/ActivityForm";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 export default observer(function ActivityDashboard() {
-    const {activityStore} = useStore();
+    const { activityStore } = useStore();
+
+    useEffect(() => {
+        if (activityStore.activityRegistry.size <= 1) activityStore.loadActivities();
+    }, [activityStore.activityRegistry.size, activityStore.loadActivities])
+
+    if(activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
+
     return (
         <Grid>
             <GridColumn width='10'>
-                <ActivityList/>
+                <ActivityList />
             </GridColumn>
             <GridColumn width='6'>
-                {activityStore.selectedActivity && !activityStore.editMode && 
-                <ActivityDetails/>}
-                {activityStore.editMode && 
-                <ActivityForm/>}
+                <h2>Activity filters</h2>
             </GridColumn>
         </Grid>
     )
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,5 +1,6 @@
 import { observer } from "mobx-react-lite";
-import React from "react";
+import React, { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 import { Button, Card, Image } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
@@ -7,9 +8,17 @@ import { useStore } from "../../../app/stores/store";
 
 export default observer(function ActivityDetails() {
 
-    const {activityStore} = useStore();
-    const {selectedActivity} = activityStore;
-    if(!selectedActivity) return <LoadingComponent/>
+    const { activityStore } = useStore();
+    const { selectedActivity } = activityStore;
+
+    const { id } = useParams<{ id: string }>();
+
+    useEffect(() => {
+        if (id) activityStore.loadActivity(id);
+    }, [id, activityStore.loadActivity])
+
+    if (activityStore.loadingInitial || !activityStore.selectedActivity) return <LoadingComponent />
+
     return (
         <Card fluid>
             <Image src={`/assets/categoryImages/${activityStore.selectedActivity?.category}.jpg`} />
@@ -24,8 +33,8 @@ export default observer(function ActivityDetails() {
             </Card.Content>
             <Card.Content extra>
                 <Button.Group widths='2'>
-                    <Button onClick={() => activityStore.openForm(activityStore.selectedActivity?.id)} basic color='blue' content='Edit' />
-                    <Button onClick={()=> activityStore.cancelSelectActivity()} basic color='grey' content='Cancel' />
+                    <Button as={Link} to={`/manage/${activityStore.selectedActivity.id}`} basic color='blue' content='Edit' />
+                    <Button as={Link} to='/activities' basic color='grey' content='Cancel' />
                 </Button.Group>
             </Card.Content>
         </Card>
