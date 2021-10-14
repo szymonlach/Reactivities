19d921634f0ea9fddb794e8c754976fc89f3de69
Szymon Lach <szymom.sp1@gmail.com>
2021-10-14 21:35:45 +0200

    End of section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -31,6 +31,16 @@ export default class ActivityStore {
         return this.activityRegistry.get(id);
     }
 
+    get groupedActivitiesByDate(){
+        return Object.entries(
+            this.activitiesByDate.reduce((activities, activity)=>{
+                const date = activity.date;
+                activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+                return activities;
+            }, {} as {[key: string]:Activity[]})
+        )
+    }
+
     loadActivities = async () => {
         this.loadingInitial = true;
         try {
