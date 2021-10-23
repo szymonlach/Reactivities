024c1a2aa837f03a07e4c5ea7cf693d63a1d2de1
Szymon Lach <szymom.sp1@gmail.com>
2021-10-23 13:25:44 +0200

    End od section 16

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -32,7 +32,7 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            var user = await _userManager.Users.Include(p => p.Photos).FirstOrDefaultAsync(x => x.Email == loginDto.Email);
             if (user == null) return Unauthorized();
 
             var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
@@ -72,7 +72,7 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<UserDto>> GetCurrentUser()
         {
-            var user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+            var user = await _userManager.Users.Include(x => x.Photos).FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
 
             return CreateUserObject(user);
         }
@@ -84,7 +84,7 @@ namespace API.Controllers
                 DisplayName = user?.DisplayName,
                 Username = user?.UserName,
                 Token = _tokenservice.CreateToken(user),
-                Image = null
+                Image = user?.Photos?.FirstOrDefault(x => x.IsMain == true)?.Url
             };
         }
     }
