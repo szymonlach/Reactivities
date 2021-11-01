c67145a93e75e243d750722f53b7d38f656b80bc
Szymon Lach <szymom.sp1@gmail.com>
2021-11-01 19:55:07 +0100

    End of section 19

---
+++ Domain/Comment.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Body { get; set; }
+        public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+        public AppUser Author { get; set; }
+        public Activity Activity { get; set; }
+    }
+}
--- client-app/src/features/profiles/ProfileAbout.tsx
+++ client-app/src/features/profiles/ProfileAbout.tsx
@@ -1,6 +1,6 @@
 import { observer } from "mobx-react-lite";
 import React, { useState } from "react";
-import { Button, Grid, Header, Tab, TextArea } from "semantic-ui-react";
+import { Button, Grid, Header, Tab } from "semantic-ui-react";
 import { Profile } from "../../app/models/profile";
 import { useStore } from "../../app/stores/store";
 import ProfileEditForm from "./ProfileEditForm";
